be7b48e942b02f4aac71b795ec3dc8d2bf3a26f5
tmtowtdi <tmtowtdi@gmail.com>
2015-07-09 17:34:41 -0400

    Moved virtualenv functions out to a separate file

---
+++ functions/virtualenvs.fish
@@ -0,0 +1,14 @@
+
+function virtmonty --description 'Turn on MontyLacuna virtenv'
+    source ~/work/python/virtualenvs/MontyLacuna/bin/activate.fish
+    alias py=~/work/python/virtualenvs/MontyLacuna/bin/python
+end
+function virtpypy --description 'Turn on PyPy virtenv'
+    source ~/work/python/virtualenvs/PyPy/bin/activate.fish
+    alias py=~/work/python/virtualenvs/PyPy/bin/python
+end
+function virtpyside --description 'Turn on Pyside virtenv'
+    source ~/work/python/virtualenvs/PySide/bin/activate.fish
+    alias py=~/work/python/virtualenvs/PySide/bin/python
+end
+
