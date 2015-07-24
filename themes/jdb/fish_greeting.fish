bf9422fec17de587bbf0aaace01b4e37d65f68e0
tmtowtdi <tmtowtdi@gmail.com>
2015-07-24 16:08:40 -0400

    De-complicated perlbrew version in greeting

--- themes/jdb/fish_greeting.fish
+++ themes/jdb/fish_greeting.fish
@@ -6,9 +6,7 @@ function fish_greeting --description 'Print the shell greeting'
     set -l system (printf "%sRunning %s%s%s on %s%s%s" $cn $cc (uname -mrs) $cn $cc (tty | sed -e 's/.*tty\(.*\)/\1/') $cn)
     set -l lsb (printf "%sDist version %s%s%s, codename %s%s%s" $cn $cc (lsb_release -ds) $cn $cc (lsb_release -cs) $cn)
     set -l datetime (printf "%sIt is %s%s%s (%s) on %s%s%s" $cn $cc (date +%T) $cn (date +%Z) $cc (date +%F) $cn)
-
-    set -l perlbrew_vers ( printf "%s" (perl -E "print(\$ARGV[1] =~ s/.*Name:\s+([\d\.]*)/\$1/r)" (perlbrew info)) )
-    set -l perlbrew (printf "%sCurrent perlbrew version in use is %s%s%s." $cn $cc $perlbrew_vers $cn)
+    set -l perlbrew (printf "%sCurrent perlbrew version in use is %s%s%s." $cn $cc $PERLBREW_PERL $cn)
 
     printf "%s" $cc
     ftext
