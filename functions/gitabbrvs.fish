ca6e6684f82be6de47db4978160e91a0bd382dbe
tmtowtdi <tmtowtdi@gmail.com>
2015-07-24 16:15:49 -0400

    handsss

---
+++ functions/gitabbrvs.fish
@@ -0,0 +1,8 @@
+
+function hands --description "Simple no-commit-message commit"
+    git commit -m handsss
+end
+
+function mas --description "Push master to origin"
+    git push origin master
+end
