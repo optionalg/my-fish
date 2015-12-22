ad0ca82114daa58239c3248a5bda012389b1124e
tmtowtdi <tmtowtdi@gmail.com>
2015-12-22 03:29:48 -0500

    added gpush and gpull functions

--- functions/gitabbrvs.fish
+++ functions/gitabbrvs.fish
@@ -3,6 +3,10 @@ function hands --description "Simple no-commit-message commit"
     git commit -m handsss
 end
 
-function mas --description "Push master to origin"
+function gpush --description "Push master to origin"
     git push origin master
+end
+
+function gpull --description "Pull master from origin"
+    git ll origin master
 end
